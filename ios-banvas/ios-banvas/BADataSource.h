97889e79c43466a819b38c328dd4a68955e1d59a
LucienLee <Lucien@Luc-MacBook-Pro.local>
2012-11-23 22:36:20 +0800

    bone

---
+++ ios-banvas/ios-banvas/BADataSource.h
@@ -0,0 +1,36 @@
+//
+//  BADataSource.h
+//  ios-banvas
+//
+//  Created by Lucien on 12/11/23.
+//  Copyright (c) 2012年 lintseju. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BADataSource : NSObject{
+    
+    NSCache* cache;
+}
+
++(BADataSource*) data;
+
+//client side
+-(NSArray*) getPersonList;
+-(NSDictionary*) getPersonInfo:(id) byPersonID;
+
+
+
+//server side
+
+//For scan class
+-(Boolean) createPersonByPersonID:(NSString*) personID;
+
+//For Collection update
+-(Boolean) updatePersonByPersonID:(NSString*) personID andTag:(NSString*)tag;
+
+-(Boolean) deletePersonByPersonID:(NSString*) personID;
+
+-(Boolean) readPersonByPersonID:(NSString*) personID;
+
+@end
---
+++ ios-banvas/ios-banvas/BADataSource.m
@@ -0,0 +1,13 @@
+//
+//  BADataSource.m
+//  ios-banvas
+//
+//  Created by Lucien on 12/11/23.
+//  Copyright (c) 2012年 lintseju. All rights reserved.
+//
+
+#import "BADataSource.h"
+
+@implementation BADataSource
+
+@end
